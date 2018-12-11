9844f886ae20e003f23c204303119305ff24565b
Juan M Barrios <j.m.barrios@gmail.com>
2018-12-10 23:25:13 -0600

    Day 9 puzzle

--- day9/day9.py
+++ day9/day9.py
@@ -1,6 +1,6 @@
 import sys
 import re
-from collections import defaultdict
+from collections import defaultdict, deque
 
 
 def solution1(input_file):
@@ -9,29 +9,41 @@ def solution1(input_file):
         no_of_players, no_of_marbles = map(int, re.findall(r'[0-9]+', l))
 
     player_score = defaultdict(int)
-    circle = [0, 1]
-    current_marble = 1
-    current_player = 1
-    for m in range(2, no_of_marbles+1):
+    circle = deque([0])
+    current_player = 0
+    for m in range(1, no_of_marbles+1):
         current_player = (current_player + 1) % no_of_players
         if m % 23 == 0:
-            current_marble = (current_marble - 7) % len(circle)
-            player_score[current_player] += m + circle.pop(current_marble)
+            circle.rotate(7)
+            player_score[current_player] += m + circle.pop()
+            circle.rotate(-1)
         else:
-            current_marble = (current_marble + 2) % len(circle)
+            circle.rotate(-1)
+            circle.append(m)
 
-            if current_marble == 0 and m % 23 !=0:
-                current_marble = len(circle)
-                circle.append(current_marble)
-            else:
-                circle.insert(current_marble, m)
-
-    print(player_score)
     return max(player_score.values())
 
 
 def solution2(input_file):
-    pass
+    with open(input_file, 'r') as f:
+        l = f.readline()
+        no_of_players, no_of_marbles = map(int, re.findall(r'[0-9]+', l))
+
+    no_of_marbles = no_of_marbles * 100
+    player_score = defaultdict(int)
+    circle = deque([0])
+    current_player = 0
+    for m in range(1, no_of_marbles+1):
+        current_player = (current_player + 1) % no_of_players
+        if m % 23 == 0:
+            circle.rotate(7)
+            player_score[current_player] += m + circle.pop()
+            circle.rotate(-1)
+        else:
+            circle.rotate(-1)
+            circle.append(m)
+
+    return max(player_score.values())
 
 
 def main(input_file, part):
